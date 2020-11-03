edf3e496acb354df0bc15abe1b50fbd0dfacb7ae
gimly93 <a.lukashevich93@gmail.com>
2020-11-03 11:03:56 +0300

    service-worker data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
+import React, { Component, useEffect } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import addNotification from 'react-push-notification';
+import { Notifications } from 'react-push-notification';
+
+const Page = ({ title }) => {
+
+  useEffect(() => {
+    window.addEventListener("offline", (event) => {
+      console.log('!!!!!!!!', event)
+      addNotification({
+            title: 'Offline',
+            subtitle: 'connection lost',
+            message: 'connection lost',
+            theme: 'darkblue',
+            native: true // when using native, your OS will handle theming.
+        })});
+
+    window.addEventListener("online", (event)=>  {
+      console.log('!!!!!!!!', event)
+      
+      addNotification({
+      title: 'online',
+      subtitle: 'connection established',
+      message: 'connection established',
+      theme: 'darkblue',
+      native: true // when using native, your OS will handle theming.
+  })});
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
+        <Notifications />
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>{title}</h2>
+      </div>
+      <p className="App-intro">
+        This is the {title} page.
+      </p>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
     </div>
   );
+} 
+
+const Home = (props) => (
+  <Page title="Home"/>
+);
+
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/settings" component={Settings}/>
+      </Router>
+    );
+  }
 }
 
 export default App;
